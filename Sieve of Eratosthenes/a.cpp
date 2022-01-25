7e02ee90fcc39e5a5b251f32f0c49340f7e96b06
swapneshcoding <98379613+swapneshcoding@users.noreply.github.com>
2022-01-25 23:05:19 +0530

    Seive_of_Eratosthenes

---
+++ Sieve of Eratosthenes/a.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<bool> Seive_of_Eratosthenes(int n) {
+	vector<bool>isPrime(n + 1, true);
+	isPrime[0] = isPrime[1] = false;
+	for (int j = 2 * 2; j <= n; j += 2) isPrime[j] = false;
+	for (int j = 3 * 3; j <= n; j += 3) isPrime[j] = false;
+	for (int i = 1;; i += 1) {
+		if ((6 * i - 1) * (6 * i - 1) > n)
+			break;
+		if (isPrime[6 * i - 1])
+			for (int j = (6 * i - 1) * (6 * i - 1); j <= n; j += 6 * i - 1) isPrime[j] = false;
+		if ((6 * i + 1) * (6 * i + 1) > n)
+			break;
+		if (isPrime[6 * i + 1])
+			for (int j = (6 * i + 1) * (6 * i + 1); j <= n; j += 6 * i + 1) isPrime[j] = false;
+	}
+	return isPrime;
+}
+
+
+int main() {
+	vector<bool>isPrime = Seive_of_Eratosthenes(1e9 + 7);
+	cout << "isPrime.size(): " << isPrime.size() << "\n";
+	cout << "isPrime[1e9+7]: " << isPrime[1e9 + 7] << "\n";
+}
+
+/*
+output:
+isPrime.size(): 1000000008
+isPrime[1e9+7]: 1
+[Finished in 72.1s]
+*/
